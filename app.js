354079da22338b1dbf5b1007fae8da77322b25b1
thamudi <tamim.hamoudi@gmail.com>
2021-05-22 16:41:47 +0300

    To Do list added for the goals

--- app.js
+++ app.js
@@ -1,2 +1,51 @@
 'use strict';
 const puppeteer = require('puppeteer');
+
+// TODO: Alright, so before writing any stupid code tamim we will need to outline some things
+
+/*
+================================================================================================
+================================================================================================
+============================================ GOALS =============================================
+
+1- We need to go to the Characters page [check]
+2- We need to access the page [check]
+3- Find the place containing all of the Data we want, in this case it the container containing 
+   the members data (name, link)
+4- Get An array of all the containers 
+   1. Loop through this containers
+   2. find all the links
+   3. go to the page
+   4. get the image and the name of the character
+   5. save the image in a folder with the character's name
+   6. go back to the previous page again; basically reset to the Characters Page
+   7. loop over
+   8. profit 
+
+
+================================================================================================
+================================================================================================
+================================================================================================
+*/
+
+const scrapeData = async (url) => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    // if (await page.$('.category-page__members')) {
+    //     if (await page.$$('.category-page__members-wrapper')) {
+    //         while(await (await page.$$('.category-page__members-wrapper')).length){
+    //             while(await(await page.$$('.category-page__member')).length){
+
+    //             }
+    //         }
+    //     }
+    // }
+
+    browser.close();
+
+}
+
+scrapeData('https://psychonauts.fandom.com/wiki/Category:Characters');
