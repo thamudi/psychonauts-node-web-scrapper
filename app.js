ed8d98765a3cea384748da0f205beaa9966872bf
thamudi <tamim.hamoudi@gmail.com>
2021-05-23 00:24:19 +0300

    Some progess was made

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const puppeteer = require('puppeteer');
 1- We need to go to the Characters page [check]
 2- We need to access the page [check]
 3- Find the place containing all of the Data we want, in this case it the container containing 
-   the members data (name, link)
-4- Get An array of all the containers 
-   1. Loop through this containers
-   2. find all the links
-   3. go to the page
+   the members data (name, link) [check]
+4- Get An array of all the containers [check]
+   1. Loop through this containers [check]
+   2. find all the links [check]
+   3. go to the page [check]
    4. get the image and the name of the character
    5. save the image in a folder with the character's name
    6. go back to the previous page again; basically reset to the Characters Page
@@ -34,23 +34,48 @@ https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
 */
 
 const scrapeData = async (url) => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+   const browser = await puppeteer.launch({ headless: false });
+   const page = await browser.newPage();
 
-    await page.goto(url);
+   await page.goto(url);
 
-    // if (await page.$('.category-page__members')) {
-    //     if (await page.$$('.category-page__members-wrapper')) {
-    //         while(await (await page.$$('.category-page__members-wrapper')).length){
-    //             while(await(await page.$$('.category-page__member')).length){
+   if ((await page.$$('li.category-page__member a'))) {
+      const queriedPageMembers = await page.$$eval('li.category-page__member',
+         links => links.map(link => {
+            const a = link.querySelector('li.category-page__member > a');
+            return {
+               name: a.innerText,
+               link: a.href
 
-    //             }
-    //         }
-    //     }
-    // }
+            }
+         }).filter(obj => !(obj.name.match(/(category|list)/gi)))
+      );
 
-    browser.close();
+
+      console.log(queriedPageMembers);
+
+      const data = [];
+
+      for (const { name, link } of queriedPageMembers) {
+         await Promise.all([
+            page.waitForNavigation(),
+            page.goto(link),
+            page.waitForSelector('.pi-item.pi-item-spacing.pi-title'),
+         ]);
+
+         const info = await page.$eval('.pi-item.pi-item-spacing.pi-title', e => e.innerText);
+
+         data.push({
+            name: name,
+            information: info,
+         });
+      }
+
+      console.log(data);
+
+   }
 
 }
+
 
 scrapeData('https://psychonauts.fandom.com/wiki/Category:Characters');
